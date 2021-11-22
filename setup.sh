2680728bbb559196e98c858f6988acd46dc11a10
test <localhost>
2021-11-21 19:13:18 -0800

    +setup.sh:Install & configure pkg's for dev env

---
+++ setup.sh
@@ -0,0 +1,6 @@
+apt-get update && apt-get upgrade &&
+apt-get install vim ctags git sqlite
+git config --global alias.cg "config --global"
+git cg alias.uncg "cg --unset"
+git cg alias.lol "log --oneline"
+
