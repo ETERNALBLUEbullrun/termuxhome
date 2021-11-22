897e6e34ccb57729b83056d25365fd3183b9beb0
test <localhost>
2021-11-22 13:39:32 -0800

    ?setup.sh:Single quotes. +git alias:undo & undoc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 apt-get update && apt-get upgrade &&
 apt-get install vim ctags git sqlite
-git config --global alias.cg "config --global"
-git cg alias.uncg "cg --unset"
-git cg alias.lol "log --oneline"
+git config --global alias.cg 'config --global'
+git cg alias.uncg 'cg --unset'
+git cg alias.lol 'log --oneline'
+git cg alias.undoc 'commit -c ORIG_HEAD'
+#git cg alias.undo 'reset HEAD~'
+git cg alias.undo '!git reset HEAD~ && echo "\"git undoc\" for git commit -c ORIG_HEAD" && :'
 
