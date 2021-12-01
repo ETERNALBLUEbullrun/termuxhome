f67511657b4c706591488349ecad686ad2ab7beb
test <localhost>
2021-11-30 19:30:20 -0800

    ?setup.sh:[pkg install openssh gh]+.ssh/id_ed25519

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+set -x
 apt-get update && apt-get upgrade &&
 apt-get install vim ctags git sqlite
 . gittest/bin/gitAlias.sh
+apt-get install openssh gh
+cp ~/../usr/etc/ssh/ssh_host_ed25519_key ~/.ssh/id_ed25519
 
