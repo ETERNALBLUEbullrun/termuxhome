743162aac0339743d29105385cb5faecb213432d
test <localhost>
2021-11-30 19:26:21 -0800

    ?setup.sh:git config into gittest/bin/gitAlias.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 apt-get update && apt-get upgrade &&
 apt-get install vim ctags git sqlite
-git config --global alias.cg 'config --global'
-git cg alias.uncg 'cg --unset'
-git cg alias.lol 'log --oneline'
-git cg alias.undoc 'commit -c ORIG_HEAD'
-#git cg alias.undo 'reset HEAD~'
-git cg alias.undo '!git reset HEAD~ && echo "\"git undoc\" for git commit -c ORIG_HEAD" && :'
+. gittest/bin/gitAlias.sh
 
