01609741ce62119608b0d74e230e7edb7fa97db0
test <localhost>
2021-12-04 21:03:40 -0800

    +proot-distro+$InstalledRootfs:Framework for proot

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ apt-get install vim ctags git sqlite
 . gittest/bin/gitAlias.sh
 apt-get install openssh gh
 cp ~/../usr/etc/ssh/ssh_host_ed25519_key ~/.ssh/id_ed25519
+apt-get install proot-distro && (
+)
 
