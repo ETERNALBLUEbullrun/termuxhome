138783770afe1c4bf35bde1d786f007ce9ae6100
Bullrun <EternalblueBullrun@gmail.com>
2023-12-05 14:02:19 -0800

    ?setup.sh `pkg install man manpages`
    'manpages' allows function lookups, ergo `man malloc`.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 set -x
 apt-get update && apt-get upgrade &&
 apt-get install vim ctags git sqlite
+pkg install man manpages
+pkg install doxygen
+pkg install dpkg #not found: dpkg-buildpackage
+pkg install apt-file &&
+	apt-file update
 . gittest/bin/gitAlias.sh
 apt-get install openssh gh
 cp ~/../usr/etc/ssh/ssh_host_ed25519_key ~/.ssh/id_ed25519
@@ -11,4 +16,7 @@ cat .vimrc >> $DebianRoot/.vimrc
 cat .bashrc >> $DebianRoot/.bashrc
 cp --parents .vim/TabComplete $DebianRoot
 )
+ssh-keygen -t ed25519 -C "you"
+eval `ssh-agent -s`
+ssh-add ./.ssh/id_ed25519
 
